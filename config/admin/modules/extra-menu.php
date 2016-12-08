7a99306e2d408a85e3d5f55ea54f7da2d83313d9
Gudin Grisha <g.gudin@kubikrubik.ru>
2016-12-08 18:01:54 +0300

    Обновлен конфиг ACL
    Signed-off-by: Gudin Grisha <g.gudin@kubikrubik.ru>

--- config/admin/modules/extra-menu.php
+++ config/admin/modules/extra-menu.php
@@ -10,24 +10,43 @@ return array(
 			'allow' => array(
 				// Module
 				array(
-					'role' => 'main',
+					'role' => 'full',
 					'resource' => 'extra_menu_controller',
 					'privilege' => 'access',
-					'assertion' => array('Acl_Assert_Module_Access', array(
-						'module' => 'extra-menu',
-					)),
 				),
-			
+					
 				// Elements
 				array(
-					'role' => 'main',
+					'role' => 'full',
 					'resource' => 'extra_menu',
 					'privilege' => 'edit',
+					'assertion' => array('Acl_Assert_Edit', array(
+						'site_id' => SITE_ID,
+					)),
+				),
+				array(
+					'role' => 'full',
+					'resource' => 'extra_menu',
+					'privilege' => 'hide',
+					'assertion' => array('Acl_Assert_Hide', array(
+						'site_id' => SITE_ID,
+						'site_id_master' => SITE_ID_MASTER
+					)),
+				),
+				array(
+					'role' => 'super',
+					'resource' => 'extra_menu',
+					'privilege' => 'fix_all',
 				),
 				array(
 					'role' => 'main',
 					'resource' => 'extra_menu',
-					'privilege' => 'fix_all',
+					'privilege' => 'fix_master',
+				),
+				array(
+					'role' => 'full',
+					'resource' => 'extra_menu',
+					'privilege' => 'fix_slave',
 				),
 			),
 			'deny' => array(
